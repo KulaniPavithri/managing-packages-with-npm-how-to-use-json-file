6c85f41569662f94bd432f76e4d4995d383dcdba
Kulani Pavithri <kulani.pavithri@gmail.com>
2022-05-04 15:51:44 +0530

    Added env file

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use("/public", express.static(__dirname + "/public"));
 
 //Serve JSON on a Specific Route
 app.get("/json", (req, res) => {
-  if (process.env.MESSAGE_STYLE===uppercase)
+  if (process.env.MESSAGE_STYLE==="uppercase")
     res.json({"message": "HELLO JSON"})
   else 
     res.json({"message": "Hello Json"})
